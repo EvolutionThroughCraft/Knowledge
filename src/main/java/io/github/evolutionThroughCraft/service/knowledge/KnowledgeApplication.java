43be29544189b255b98f13280ac1b0a5c4c193fd
Dwin <dwin.e357@gmail.com>
2018-12-30 12:09:19 -0500

    First commit

---
+++ src/main/java/io/github/evolutionThroughCraft/service/knowledge/KnowledgeApplication.java
@@ -0,0 +1,14 @@
+package io.github.evolutionThroughCraft.service.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeApplication.class, args);
+	}
+
+}
+
